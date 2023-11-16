30236a526d6119e52edaf45d78eefe35a7dcd14d
jeremiahwong007 <73967834+jeremiahwong007@users.noreply.github.com>
2023-11-16 11:40:57 -0500

    fix mobile background

--- src/components/BackgroundVideo/index.js
+++ src/components/BackgroundVideo/index.js
@@ -3,7 +3,7 @@ import './styles.css'
 
 const BackgroundVideo = () => {
     return <>
-        <video className='my-video' autoPlay loop muted playsinline>
+        <video className='my-video hidden sm:block' autoPlay loop muted>
             <source src={background_vid} type="video/mp4"></source>
         </video>
     </>
