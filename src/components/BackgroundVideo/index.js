b0a120d38300d695b4ffd4235d173180574a118b
jeremiahwong007 <73967834+jeremiahwong007@users.noreply.github.com>
2023-11-16 03:37:44 -0500

    fix background video on mobile

--- src/components/BackgroundVideo/index.js
+++ src/components/BackgroundVideo/index.js
@@ -3,7 +3,7 @@ import './styles.css'
 
 const BackgroundVideo = () => {
     return <>
-        <video className='my-video' autoPlay loop muted>
+        <video className='my-video' autoPlay loop muted playsinline>
             <source src={background_vid} type="video/mp4"></source>
         </video>
     </>
