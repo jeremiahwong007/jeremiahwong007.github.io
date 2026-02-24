ba80ab597bd8743030e68b6d9259c369974f9116
Jeremy Wong <73967834+jeremiahwong007@users.noreply.github.com>
2026-02-24 16:15:29 -0500

    test video background on phone

--- src/components/BackgroundVideo/index.js
+++ src/components/BackgroundVideo/index.js
@@ -3,7 +3,7 @@ import './styles.css'
 
 const BackgroundVideo = () => {
     return <>
-        <video className='my-video hidden sm:block' autoPlay loop muted>
+        <video className='my-video' autoPlay loop muted playsInline poster="/mobile_background.png">
             <source src={background_vid} type="video/mp4"></source>
         </video>
     </>
