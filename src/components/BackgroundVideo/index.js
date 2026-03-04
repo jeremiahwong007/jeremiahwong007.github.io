1cd866741f7a42e9df52cd0868350fc65ffec79b
Jeremy Wong <73967834+jeremiahwong007@users.noreply.github.com>
2026-03-03 23:33:16 -0500

    test

--- src/components/BackgroundVideo/index.js
+++ src/components/BackgroundVideo/index.js
@@ -3,7 +3,7 @@ import './styles.css'
 
 const BackgroundVideo = () => {
     return <>
-        <video className='my-video' autoPlay loop muted playsInline poster="/mobile_background.png">
+        <video className='my-video' autoPlay loop muted playsInline webkit-playsinline poster="/mobile_background.png">
             <source src={background_vid} type="video/mp4"></source>
         </video>
     </>
