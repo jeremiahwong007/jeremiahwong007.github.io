7044e1c04b8b953f49a0f719591e8ac54ae7a3e1
Jeremy Wong <73967834+jeremiahwong007@users.noreply.github.com>
2026-03-21 13:25:03 -0400

    dynamic

--- src/components/BackgroundVideo/index.js
+++ src/components/BackgroundVideo/index.js
@@ -6,6 +6,8 @@ const AnimatedBackground = () => {
             <div className="gradient-orb orb-1"></div>
             <div className="gradient-orb orb-2"></div>
             <div className="gradient-orb orb-3"></div>
+            <div className="gradient-orb orb-4"></div>
+            <div className="gradient-orb orb-5"></div>
         </div>
     )
 }
